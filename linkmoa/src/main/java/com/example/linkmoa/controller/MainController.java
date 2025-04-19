c1202192fd2526090ebaa70710cc0143af53f185
황광회 <jack7141@nate.com>
2025-04-19 15:27:27 +0900

    DB setting 시작

--- linkmoa/src/main/java/com/example/linkmoa/controller/MainController.java
+++ linkmoa/src/main/java/com/example/linkmoa/controller/MainController.java
@@ -2,7 +2,9 @@ package com.example.linkmoa.controller;
 
 import com.example.linkmoa.dto.ItemDTO;
 import com.example.linkmoa.dto.RespDTO;
+import com.example.linkmoa.service.MainService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class MainController {
+    @Autowired
+    private MainService mainService;
 
     @GetMapping("/")
     public String main(){
@@ -20,10 +24,17 @@ public class MainController {
 
     @PostMapping("/user")
     public RespDTO user(@RequestBody ItemDTO itemDTO) {
-        log.info("itemDTO: {}", itemDTO);
+        boolean is_success = mainService.registerItem(itemDTO);
+        if (is_success) {
+            RespDTO respDTO =  new RespDTO();
+             log.info("itemDTO: {}", itemDTO);
+            respDTO.setCode(200);
+            respDTO.setMsg("success");
+            return respDTO;
+        }
         RespDTO respDTO =  new RespDTO();
-        respDTO.setCode(200);
-        respDTO.setMsg("success");
+        respDTO.setCode(400);
+        respDTO.setMsg("failed");
         return respDTO;
     }
 }
